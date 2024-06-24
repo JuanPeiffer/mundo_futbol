03d58436d3e73987baf936d12828de38cca3d2ee
Juan <juampybj2013@gmail.com>
2024-06-23 21:53:18 -0300

    Creacion basica de Usuarios

--- equipos/views.py
+++ equipos/views.py
@@ -1,8 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 def about(request):
     return HttpResponse("Sobre mundo")
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+        'form': UserCreationForm
+    })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            # Register User
+            try:
+                user = User.objects.create_user(
+                username=request.POST['username'],
+                password=request.POST['password1'])
+                user.save()
+                return HttpResponse('Usuario creado correctamente')
+            except:
+                return HttpResponse('Error al crear usuario')
+        else:
+            return HttpResponse('Las contraseñas no coinciden')
