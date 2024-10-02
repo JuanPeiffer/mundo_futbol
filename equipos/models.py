95f0392d837a2f1437d60fa220ef0320e9342d88
Juan <juampybj2013@gmail.com>
2024-10-02 12:13:02 -0300

    add campos equipofutbol model

--- equipos/models.py
+++ equipos/models.py
@@ -15,8 +15,11 @@ class Selecciones(models.Model):
 
 
 class EquipoFutbol(models.Model):
-    nombre = models.CharField(max_length=150)
-    url = models.CharField(max_length=150, blank=True, null=True)
+    nombre = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='logos/', blank=True, null=True)
+    historia = models.TextField(blank=True, null=True)
+    fecha_fundacion = models.DateField(blank=True, null=True)
+    ciudad = models.CharField(max_length=100, blank=True, null=True)
     
     def __str__(self):
         return self.nombre
